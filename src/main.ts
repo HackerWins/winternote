29a2649bb44ee6c2510bba17ed5c3fd338d97a5a
easylogic <cyberuls@gmail.com>
2019-01-02 00:00:36 +0900

    Update main function

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { Editor } from './editor';
 
-export function create(place: Element): void {
+export function create(place: Element): Editor {
   const editor = new Editor({
     container: place
   });
+
+  return editor;
+}
+
+export function main(selector) {
+  const editor = create(document.querySelector(selector));
   // tslint:disable-next-line
   console.log(editor.read('editor.name'));  
 }
